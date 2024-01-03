720cc008c821e5198f70c91b695cd852f0f04678
Faizan Qaiser <faizanqaier9@gmail.com>
2024-01-03 12:16:23 +0500

    Custom Hud Input
    Added an input manager, Hud Button, hud button attribute and an attribute drawer.
    
    The input manager can now connect to any button on the HUD

---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Phezu.Util;
+using System;
+
+public class InputManager : Singleton<InputManager>
+{
+    [SerializeField] private string[] m_HudButtons;
+
+    public string[] HudButtons {
+        get {
+            string[] result = new string[m_HudButtons.Length];
+            for (int i = 0; i < result.Length; i++)
+                result[i] = m_HudButtons[i];
+            return result;
+        }
+    }
+
+    private bool[] m_HudButtonsState;
+
+    private void Awake() {
+        if (m_HudButtons == null)
+            return;
+        m_HudButtonsState = new bool[m_HudButtons.Length];
+    }
+
+#if UNITY_EDITOR
+
+    [SerializeField] private List<bool> m_DebugInputs;
+
+    private void Start() {
+        for (int i = 0; i < m_HudButtons.Length; i++)
+            m_DebugInputs.Add(false);
+    }
+    private void Update() {
+        for (int i = 0; i < m_DebugInputs.Count; i++)
+            m_DebugInputs[i] = m_HudButtonsState[i];
+    }
+
+#endif
+
+    public void SetHudButton(int hudButtonIndex, bool state) {
+        m_HudButtonsState[hudButtonIndex] = state;
+    }
+
+    public bool GetHudButton(int hudButtonIndex) {
+        return m_HudButtonsState[hudButtonIndex];
+    }
+
+    public bool[] GetHudButtons() {
+        bool[] buttons = new bool[m_HudButtonsState.Length];
+
+        for (int i = 0; i < buttons.Length; i++) {
+            buttons[i] = m_HudButtonsState[i];
+        }
+
+        return buttons;
+    }
+}
---
+++ Assets/Scripts/UI/HudButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HudButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+    [HudButton] public int m_ButtonType;
+
+    private InputManager m_InputManager;
+
+    private void Start() {
+        m_InputManager = InputManager.Instance;
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        m_InputManager.SetHudButton(m_ButtonType, true);
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        m_InputManager.SetHudButton(m_ButtonType, false);
+    }
+}
